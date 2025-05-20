2f2469abdd5299aa1c1cb4f65e93d1a801ec2c95
stack-overflow-maxxer <adamgajdosik47@gmail.com>
2025-05-20 18:03:40 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+const users = []
+
+const fs = require('fs');
+const path = require('path');
+const USERS_FILE = path.join(__dirname, 'users.json');
+
+const cors = require('cors');
+app.use(cors());
+
+
+function loadUsers() { 
+    if (!fs.existsSync(USERS_FILE)) return [];
+    const data = fs.readFileSync(USERS_FILE);
+    return JSON.parse(data)
+}
+
+function saveUsers(users) { 
+    fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2) )
+}
+
+
+app.use(express.json())
+
+app.get('/users', (req, res) =>{
+    res.json(users)
+})
+
+app.post('/users/register', (req, res) =>{ 
+    const users = loadUsers();
+
+    console.log('Form submitted');
+
+    if (users.find(u => u.name === req.body.name)) {
+        return res.status(400).send('User already exists');
+    }
+
+    const user = {name: req.body.name, password: req.body.password,
+        lenght: req.body.lenght_p, angle: req.body.angle
+    }
+    users.push(user)
+    saveUsers(users)
+    res.status(201).send("User registred")
+})
+
+
+
+
+app.post('/users/login', (req, res) => {
+    const users = loadUsers();
+    const user = users.find(u => u.name === req.body.name);
+
+    if (!user) {
+        return res.status(400).send("1");
+    }
+
+    if (req.body.password === user.password) {
+        res.send("Login successful");
+    } else {
+        return res.status(400).send("1");
+    }
+});
+
+
+app.listen(3000)
